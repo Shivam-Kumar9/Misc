d78442009e49e7e1738c849ba397bc119e031b3f
Shivam-Kumar9 <shivamkumar1019988@gmail.com>
2024-06-29 00:15:37 +0530

    misc

---
+++ portfolio before/portfolio sk/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Fronted Developer", "Web Developer","Problam Solver"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
